daafd343db8198cd4c8aa5b472cd24c5c011d59d
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 20:30:32 -0500

    Config_Mclientes

--- app/Models/Clientes.php
+++ app/Models/Clientes.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Clientes extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nombre', 'apellido', 'email', 'telefono'
+    ];
+
+    public function entradas()
+    {
+        return $this->hasMany(Entradas::class);
+    }
 }
