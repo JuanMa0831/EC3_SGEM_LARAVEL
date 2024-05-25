91c6ae82ac13a170d1e39d94c15fc12424d6e5f0
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 20:29:40 -0500

    Config_Mentradas

--- app/Models/Entradas.php
+++ app/Models/Entradas.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Entradas extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'concierto_id', 'cliente_id', 'precio', 'fecha_compra'
+    ];
+
+    public function concierto()
+    {
+        return $this->belongsTo(Conciertos::class);
+    }
+
+    public function cliente()
+    {
+        return $this->belongsTo(Clientes::class);
+    }
 }
