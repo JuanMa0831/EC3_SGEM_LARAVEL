310aa6d14f40837218d7fa9c51d80818c02bf2c9
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 19:27:21 -0500

    Config_Table_Clientes

--- database/migrations/2024_05_25_001550_create_clientes.php
+++ database/migrations/2024_05_25_001550_create_clientes.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email')->unique();
+            $table->string('telefono');
             $table->timestamps();
         });
     }
