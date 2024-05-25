ae536083d3292589d3ba4b5069c8e301307326fa
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 20:27:46 -0500

    Config_Mconciertos

--- app/Models/Conciertos.php
+++ app/Models/Conciertos.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Conciertos extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nombre', 'fecha', 'locacion_id', 'estado', 'cantidad_entradas', 'entradas_vendidas'
+    ];
+
+    public function locacion()
+    {
+        return $this->belongsTo(Locaciones::class);
+    }
+
+    public function entradas()
+    {
+        return $this->hasMany(Entradas::class);
+    }
+
+    public function artistas()
+    {
+        return $this->belongsToMany(Artistas::class, 'concierto_artista');
+    }
 }
