962b5e1baa513abd3cad2e29d719c1abc159586b
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 20:31:35 -0500

    Config_Mlocaciones

--- app/Models/Locaciones.php
+++ app/Models/Locaciones.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Locaciones extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nombre', 'direccion', 'capacidad'
+    ];
+
+    public function conciertos()
+    {
+        return $this->hasMany(Conciertos::class);
+    }
 }
