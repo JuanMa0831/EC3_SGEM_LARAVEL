77faabb0f096256efcbfc528eaba5ce6061c9094
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-05-24 19:28:30 -0500

    Config_Table_Locaciones

--- database/migrations/2024_05_25_001640_create_locaciones.php
+++ database/migrations/2024_05_25_001640_create_locaciones.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('locaciones', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('capacidad');
             $table->timestamps();
         });
     }
